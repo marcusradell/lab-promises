020591c764142fac50c1171606accc0db1b4d302
Marcus Rådell <marcus@radell.net>
2024-10-16 10:53:45 +0200

    create a simple test that runs a chain of promises with a nested promise

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,17 @@
 import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 
-test("run a chain of promises", () => {
-  deepEqual(1, 2);
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+test("run a chain of promises with a nested promise", () => {
+  return Promise.resolve([1, 2, 3])
+    .then((data) => {
+      return sleep(5000).then(() => data);
+    })
+    .then((data) => {
+      return data.map((value) => value + 1);
+    })
+    .then((data) => {
+      deepEqual(data, [2, 3, 4]);
+    });
 });
